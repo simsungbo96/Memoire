18e0461f3215447003238de6bed8c9178fe40e5c
master <simsungbo96@gmail.com>
2022-01-14 13:58:58 +0900

    bottmo navi view / viewpager2

--- app/src/main/java/com/sbsj/memosbsj/activity/MainActivity.kt
+++ app/src/main/java/com/sbsj/memosbsj/activity/MainActivity.kt
@@ -1,32 +1,49 @@
 package com.sbsj.memosbsj.activity
 
+
+import DepthPageTransformer
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.sbsj.memosbsj.R
 import com.sbsj.memosbsj.adapter.ViewPagerAdapter
 import com.sbsj.memosbsj.data.DataPage
 
 
+
 class MainActivity : AppCompatActivity() {
 
     lateinit var topViewPager2 : ViewPager2
+    lateinit var tabLayout :TabLayout
+    var zoomOutPageTransformer = DepthPageTransformer()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         topViewPager2 = findViewById(R.id.top_viewpager2)
+        tabLayout =findViewById(R.id.tabLayout)
         init()
 
     }
 
     fun init()
     {
-        var list : ArrayList<DataPage> = ArrayList();
-        list.add(DataPage(android.R.color.holo_blue_bright,"1 Page"));
-        list.add(DataPage(android.R.color.white,"2 Page"));
-        list.add(DataPage(android.R.color.holo_orange_dark,"3 Page"));
-        list.add(DataPage(android.R.color.holo_blue_dark,"4 Page"));
+
+        val list : ArrayList<DataPage> = ArrayList();
+        list.add(DataPage(Color.BLUE,"1 Page"));
+        list.add(DataPage(Color.CYAN,"2 Page"));
+        list.add(DataPage(Color.MAGENTA,"3 Page"));
+
         topViewPager2.adapter = ViewPagerAdapter(list)
+
+
+        topViewPager2.orientation=ViewPager2.ORIENTATION_HORIZONTAL
+        topViewPager2.setPageTransformer(zoomOutPageTransformer)
+        TabLayoutMediator(tabLayout, topViewPager2) { tab, position ->
+            //Some implementation
+        }.attach()
     }
 
 }
---
+++ app/src/main/java/com/sbsj/memosbsj/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,36 @@
+package com.sbsj.memosbsj.adapter
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sbsj.memosbsj.R
+import com.sbsj.memosbsj.data.DataPage
+import com.sbsj.memosbsj.adapter.ViewPagerAdapter.ViewHolderPage
+
+
+
+
+
+class ViewPagerAdapter(dataList: ArrayList<DataPage>) : RecyclerView.Adapter<ViewPagerAdapter.ViewHolderPage>() {
+    var item = dataList
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolderPage((parent))
+
+    override fun getItemCount(): Int = item.size
+
+    override fun onBindViewHolder(holder: ViewHolderPage, position: Int) {
+        holder.title.text = item[position].title
+        holder.background.setBackgroundColor(item[position].color)
+
+
+    }
+    inner class ViewHolderPage(parent: ViewGroup) : RecyclerView.ViewHolder
+        (LayoutInflater.from(parent.context).inflate(R.layout.item_viewpager, parent, false)){
+            val title = itemView.findViewById<TextView>(R.id.tv_title)
+            val background = itemView.findViewById<RelativeLayout>(R.id.rl_layout)
+
+    }
+
+}
