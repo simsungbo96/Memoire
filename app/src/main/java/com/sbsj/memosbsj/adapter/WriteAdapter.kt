904a2524009db747b714ca1ad31670777dca28ef
SIM SUNG BO <simsungbo96@gmail.com>
2022-01-24 21:30:34 +0900

    수정 view model

--- app/src/main/java/com/sbsj/memosbsj/adapter/WriteAdapter.kt
+++ app/src/main/java/com/sbsj/memosbsj/adapter/WriteAdapter.kt
@@ -4,45 +4,46 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.sbsj.memosbsj.R
 import com.sbsj.memosbsj.data.WrittenData
-import com.sbsj.memosbsj.fragment.EditFragment
+import com.sbsj.memosbsj.databinding.ItemWrittenlistBinding
 import com.sbsj.memosbsj.viewmodel.MainViewModel
 
 class WriteAdapter internal constructor(context: Context ,var viewModel : MainViewModel) :
     RecyclerView.Adapter<WriteAdapter.ViewHolder>() {
 
-    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
     private var writtenDatas = emptyList<WrittenData>() // Cached copy of words
 
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var txtTitle: TextView = itemView.
-        var txtDate: TextView = itemView.writtenList_ll_tv_title
-
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = inflater.inflate(R.layout.item_written_list, parent, false)
-        return ViewHolder(itemView)
+        val writtenlistBinding = ItemWrittenlistBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(writtenlistBinding)
     }
+
+    class ViewHolder(private val writtenlistBinding: ItemWrittenlistBinding) :RecyclerView.ViewHolder(writtenlistBinding.root){
+        fun bind(writtenDatas: List<WrittenData>){
+            writtenlistBinding.writtenListLlTvTitle.text = writtenDatas[adapterPosition].title
+            writtenlistBinding.writtenListLlTvDate.text = writtenDatas[adapterPosition].date
+
+        }
+    }
+
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val writtenData = writtenDatas[position]
-        holder.txtTitle.text = writtenData.title
-        holder.txtDate.text = writtenData.date
+        holder.bind(writtenDatas)
     }
+    override fun getItemCount() = writtenDatas.size
 
-    internal fun setWrittenData(writtenDatas : List<WrittenData>) {
+    fun setWrittenData(writtenDatas : List<WrittenData>) {
         this.writtenDatas = writtenDatas
         notifyDataSetChanged()
     }
-    override fun getItemCount() = writtenDatas.size
 }
 
-
+//
 //class WriteAdapter(private val context: Context) : RecyclerView.Adapter<WriteAdapter.ViewHolder>() {
 //
 //    var datas = mutableListOf<WrittenData>()
